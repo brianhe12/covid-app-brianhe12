bb0364dbe736645ba7283d3502e70442770250d0
brianhe12 <brian.he17@gmail.com>
2020-04-11 20:29:42 -0400

    update

--- covidProject/App.js
+++ covidProject/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <MapView
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+    style={{ flex: 1}}
+  />
   );
 }
 
